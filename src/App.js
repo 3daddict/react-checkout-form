307d0872e32ac3208e1db0f926178540b5a4cb3d
Michael Salvati <print@3daddict.com>
2019-01-15 10:56:35 -0800

    header component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Header />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/styles.scss';
 import * as serviceWorker from './serviceWorker';
 

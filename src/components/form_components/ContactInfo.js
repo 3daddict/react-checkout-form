fadbb1c6cb89502e9339bca42ccb621d7bf8e484
Michael Salvati <print@3daddict.com>
2019-01-16 11:50:26 -0800

    contactInfo toggle added to billing address

--- src/components/form_components/ContactInfo.js
+++ src/components/form_components/ContactInfo.js
@@ -8,85 +8,80 @@ import PlusIcon from '../../assets/images/_ionicons_svg_md-add.png';
 export default class FormContactInfo extends React.Component {
   render() {
     return (
-      <Form className="container-fluid contact-info-container">
-        <h2 className="mb-3">Contact Information</h2>
-        {/* START */}
-        
-        {/* END */}
-		<Toggle>
-            {({ on, off, toggle }) => (
-            <div className="toggle-container">
-                {on && <div>
-						<Alert color="success">You contact info has been saved.</Alert>
-						<div className="button-right">
-							<Button onClick={toggle} color="secondary">Edit</Button>
-						</div>
-					</div>}
-                {off && <div>
-					<FormGroup>
-          <Input type="email" name="co_email" id="coContactEmail" placeholder="Email (For Order Confirmation)" />
-        </FormGroup>
-        <Row form>
-          <Col md={6}>
-            <FormGroup>
-              <Input type="text" name="co_first_name" id="coFirstName" placeholder="First name" />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <Input type="text" name="co_last_name" id="coLastName" placeholder="Last name" />
-            </FormGroup>
-          </Col>
-        </Row>
-        <FormGroup>
-          <Input type="text" name="co_address" id="coAddress" placeholder="Address" />
-        </FormGroup>
-        <FormGroup>
-            <Toggle>
-            {({ on, off, toggle }) => (
-            <div className="toggle-container address2-text">
-                {on && <Input type="text" name="co_address2" id="coAddress2" placeholder="Apartment, suite, etc. (optional)" />}
-                {off && <div onClick={toggle}><p className="p-xs"><img className="toggle-ico" src={PlusIcon} alt="expand icon"/> Add Address line 2</p></div>}
-            </div>
-            )}
-            </Toggle>
-        </FormGroup>
-        <FormGroup>
-        <Input type="text" name="co_city" id="coCity" placeholder="City" />
-        </FormGroup>
-        <Row form>
-          <Col md={5}>
-            <FormGroup className="dropdown-container">
-                <Label for="coCountry">Country</Label>
-                <CountryOptions />
-            </FormGroup>
-          </Col>
-          <Col md={4}>
-            <FormGroup className="dropdown-container">
-                <Label for="coState">State</Label>
-                <StateOptions />
-            </FormGroup>
-          </Col>
-          <Col md={3}>
-            <FormGroup>
-            <Label for="coZip">Zip</Label>
-              <Input className="zip-input" type="text" name="co_zipcode" id="coZipCode" placeholder="ZIP Code" />
-            </FormGroup>
-          </Col>
-        </Row>
-		<FormGroup>
-			<Input type="phone" name="co_phone" id="coPhone" placeholder="Phone (For Shipping Updates)" />
-        </FormGroup>
-						<div className="button-right">
-							<Button onClick={toggle} color="primary">Save</Button>
-						</div>
-				</div>}
-            </div>
-			
-			)}
-			
-            </Toggle>
-      </Form>
+		<Form className="container-fluid contact-info-container">
+			<h2 className="mb-3">Contact Information</h2>
+			<Toggle>
+				{({ on, off, toggle }) => (
+					<div className="toggle-container">
+						{on && <div>
+							<Alert color="success">You contact info has been saved.</Alert>
+							<div className="button-right">
+								<Button onClick={toggle} color="secondary">Edit</Button>
+							</div>
+						</div>}
+						{off && <div>
+							<FormGroup>
+								<Input type="email" name="co_email" id="coContactEmail" placeholder="Email (For Order Confirmation)" />
+							</FormGroup>
+							<Row form>
+								<Col md={6}>
+									<FormGroup>
+										<Input type="text" name="co_first_name" id="coFirstName" placeholder="First name" />
+									</FormGroup>
+								</Col>
+								<Col md={6}>
+									<FormGroup>
+										<Input type="text" name="co_last_name" id="coLastName" placeholder="Last name" />
+									</FormGroup>
+								</Col>
+							</Row>
+							<FormGroup>
+								<Input type="text" name="co_address" id="coAddress" placeholder="Address" />
+							</FormGroup>
+							<FormGroup>
+								<Toggle>
+									{({ on, off, toggle }) => (
+										<div className="toggle-container address2-text">
+											{on && <Input type="text" name="co_address2" id="coAddress2" placeholder="Apartment, suite, etc. (optional)" />}
+											{off && <div onClick={toggle}><p className="p-xs"><img className="toggle-ico" src={PlusIcon} alt="expand icon" /> Add Address line 2</p></div>}
+										</div>
+									)}
+								</Toggle>
+							</FormGroup>
+							<FormGroup>
+								<Input type="text" name="co_city" id="coCity" placeholder="City" />
+							</FormGroup>
+							<Row form>
+								<Col md={5}>
+									<FormGroup className="dropdown-container">
+										<Label for="coCountry">Country</Label>
+										<CountryOptions />
+									</FormGroup>
+								</Col>
+								<Col md={4}>
+									<FormGroup className="dropdown-container">
+										<Label for="coState">State</Label>
+										<StateOptions />
+									</FormGroup>
+								</Col>
+								<Col md={3}>
+									<FormGroup>
+										<Label for="coZip">Zip</Label>
+										<Input className="zip-input" type="text" name="co_zipcode" id="coZipCode" placeholder="ZIP Code" />
+									</FormGroup>
+								</Col>
+							</Row>
+							<FormGroup>
+								<Input type="phone" name="co_phone" id="coPhone" placeholder="Phone (For Shipping Updates)" />
+							</FormGroup>
+							<div className="button-right">
+								<Button onClick={toggle} color="primary">Save</Button>
+							</div>
+						</div>}
+					</div>
+				)}
+			</Toggle>
+		</Form>
     );
   }
 }
